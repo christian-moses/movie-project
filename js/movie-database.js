9ad8f75259f65af9b0332f1626dafc3ba67ba287
cruby9339@gmail.com <christian.ruby72@gmail.com>
2021-06-17 12:04:15 -0500

    adds adding movies object

--- js/movie-database.js
+++ js/movie-database.js
@@ -1,17 +1,52 @@
-
-fetch("https://leaf-lofty-vulture.glitch.me/movies")
-	.then(response => {
-		response.json()
-			.then(data => {
-				appendAllMovieData(data)
-				console.log(data)
-				})
-			})
-
-
-
+function getMovieData() {
+	fetch("https://leaf-lofty-vulture.glitch.me/movies")
+		.then(response => {
+			response.json()
+					.then(data => {
+						appendAllMovieData(data)
+						console.log(data)
+					})
+		})
+}
 
 
+window.onload = (event) => {
+	$("#loading")
+		.hide("slow")
+	getMovieData()
+}
+
+
+
+function addMovies(){
+	const url = 'https://leaf-lofty-vulture.glitch.me/movies'
+	let movieRating = $('#rating')
+		.val()
+	let movieTitle = $('#title')
+		.val()
+
+	let newMovie = {
+		title: movieTitle,
+		rating: movieRating
+	}
+	const options = {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(newMovie),
+	}
+
+	fetch(url,options).then()
+}
+
+
+
+$('#submit')
+	.on('click', function (e) {
+		e.preventDefault()
+		addMovies()
+	})
 
 
 
